1b8924ca652d54de82521f0c0b6d4d5326bdde79
Matheus <matheusafinovicz@gmail.com>
2021-02-16 16:30:49 -0300

    refactor

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ client = discord.Client()
 commands = ['!help -> Escrevo uma lista com meus comandos',
             '!wiki "algo" -> Busco algo na wikipedia para você!',
             '!salve -> Te mando um salve!',
-            '!tempo "cidade" -> Te informo o tempo em uma cidade']
+            '!tempo "cidade" -> Te informo o tempo de uma cidade']
 
 
 @client.event
@@ -36,16 +36,8 @@ async def on_message(message):
 
     if message.content.startswith('!tempo'):
         query = message.content[7:]
-        if query == ''*len(query):
-            await message.channel.send('A cidade não pode ser vazia.')
-        else:
-            response = weather(query)
-            string = f'{query.upper()}:\n' \
-                     f'Condição: {response["description"]}\n' \
-                     f'Temperatura atual: {response["temp"]}ºC    Sensação Térmica: {response["feels_like"]}ºC\n' \
-                     f'Humidade: {response["humidity"]}%'
-
-            await message.channel.send(string)
+        response = weather(query)
+        await message.channel.send(response)
 
 with open('keys/disc_api_key') as archive:
     for key in archive:
