a0f907c70ca159cefc1128761ca5d2a81b44dd2e
Matheus <matheusafinovicz@gmail.com>
2021-03-05 22:16:21 -0300

    search tests refactor

--- tests/test_search.py
+++ tests/test_search.py
@@ -4,22 +4,22 @@ from apis.search import search
 
 class SearchTestCase(unittest.TestCase):
 
-    def test_no_param_returns_Type_error(self):
+    def test_no_param_returns_TypeError(self):
         with self.assertRaises(TypeError):
             search()
 
-    def test_empty_string_returns_empty_search_error(self):
+    def test_empty_string_returns_empty_SearchError(self):
         query = ['', '    ']
         for string in query:
             with self.subTest():
                 self.assertEqual(search(string), 'A busca não pode ser vazia.')
 
-    def test_multiple_results_returns_wikipedia_exceptions_disambiguation_error(self):
+    def test_multiple_results_returns_wikipedia_exceptions_DisambiguationError(self):
         query = 'gremio'
         self.assertEqual(search(query), 'Encontrei muitos resultados para tal busca, por favor, '
                                         'seja mais específico.')
 
-    def test_unexisting_query_returns_wikipedia_exceptions_page_error(self):
+    def test_unexisting_query_returns_wikipedia_exceptions_PageError(self):
         query = 'dhsaudhas'
         self.assertEqual(search(query), 'Não encontrei nenhuma página relacionada a essa busca.')
 
@@ -27,26 +27,20 @@ class SearchTestCase(unittest.TestCase):
         query = 'animals'
         self.assertIsInstance(search(query), str)
 
-    def test_a_empty_dict_returns_no_param(self):
-        query = {}
-        self.assertEqual(search(query), 'Não encontrei nenhum parâmentro para buscar sobre.')
+    def test_empty_arrays_return_no_param_error(self):
+        queries = [{}, [], ()]
+        for query in queries:
+            with self.subTest():
+                self.assertEqual(search(query), 'Não encontrei nenhum parâmentro para buscar sobre.')
 
-    def test_a_empty_list_returns_no_param(self):
-        query = []
-        self.assertEqual(search(query), 'Não encontrei nenhum parâmentro para buscar sobre.')
-
-    def test_a_empty_tuple_returns_no_param(self):
-        query = ()
-        self.assertEqual(search(query), 'Não encontrei nenhum parâmentro para buscar sobre.')
-
-    def test_ints_returns_Type_Error(self):
+    def test_ints_returns_TypeError(self):
         query = [-7, 0, 3]
         for number in query:
             with self.subTest():
                 with self.assertRaises(TypeError):
                     search(number)
 
-    def test_floats_returns_Type_Error(self):
+    def test_floats_returns_TypeError(self):
         query = [-0.7, 0.21, 1.93]
         for number in query:
             with self.subTest():
